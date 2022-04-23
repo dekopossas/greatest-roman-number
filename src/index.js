1cdc226735722fdb99edb45c1dd13ccc0bf00dfb
DekoPossas <andrepossaster@gmail.com>
2022-04-22 22:27:41 -0300

    done

--- src/index.js
+++ src/index.js
@@ -9,18 +9,33 @@ const { ApolloServer, gql } = require('apollo-server');
 
 const typeDefs = gql`
   type User {
+    _id: ID!
     nome: String!
     email: String!
     active: Boolean!
   }
 
+  type Post {
+    _id: ID!
+    title: String!
+    content: String!
+    author: User!
+  }
+
   type Query {
     hello: String
+    users: [User!]!
   }
 `;
 const resolvers = {
   Query: {
     hello: () => 'Hello World',
+    users: () => [
+      { _id: String(Math.random()), name: 'John', email: 'john@example.com', active: true },
+      { _id: String(Math.random()), name: 'Deko', email: 'deko@example.com', active: true },
+      { _id: String(Math.random()), name: 'Mina', email: 'mina@example.com', active: true },
+      { _id: String(Math.random()), name: 'Pepe', email: 'pepe@example.com', active: true },
+    ],
   },
 };
 
